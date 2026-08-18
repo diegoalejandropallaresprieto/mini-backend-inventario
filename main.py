45a5e6ef89f5ee1edea1af94288d1f3e3425a5c1
Pallas66 <alejandropallares01@gmail.com>
2026-08-18 12:35:57 -0600

    password

---
+++ main.py
@@ -0,0 +1,27 @@
+# main.py
+from fastapi import FastAPI, HTTPException
+from app.domain.models import Producto
+from app.infrastructure.database import ProductoRepositoryPostgres
+from app.services.agent import compilar_agente_stock
+
+app = FastAPI(title="Mini Backend de Inventario")
+
+# Inyección de dependencias manual y limpia
+repo = ProductoRepositoryPostgres()
+agente = compilar_agente_stock()
+
+@app.post("/productos/")
+def crear_producto(producto: Producto):
+    try:
+        nuevo_producto = repo.guardar_producto(producto)
+        return {"mensaje": "Producto guardado", "producto": nuevo_producto}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@app.get("/consultar-agente/{nombre_producto}")
+def consultar_stock_con_agente(nombre_producto: str):
+    # Ejecutamos el flujo de LangGraph
+    estado_inicial = {"producto_consultado": nombre_producto}
+    resultado = agente.invoke(estado_inicial)
+    
+    return {"agente_dice": resultado["mensaje_final"]}
