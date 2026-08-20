be5822ca9ab5e214007744033cb2126849d3a379
Pallas66 <alejandropallares01@gmail.com>
2026-08-20 14:19:48 -0600

    def guardar_producto

--- app/infrastructure/database.py
+++ app/infrastructure/database.py
@@ -1,4 +1,3 @@
-# app/infrastructure/database.py
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import declarative_base, sessionmaker
 from app.domain.models import Producto, IProductoRepository
@@ -18,18 +17,22 @@ class ProductoORM(Base):
 
 Base.metadata.create_all(bind=engine)
 
-# Implementación concreta del Repositorio
 class ProductoRepositoryPostgres(IProductoRepository):
     def __init__(self):
         self.db = SessionLocal()
 
     def guardar_producto(self, producto: Producto) -> Producto:
         db_producto = ProductoORM(nombre=producto.nombre, stock=producto.stock, categoria_id=producto.categoria_id)
-        self.db.add(db_producto)
-        self.db.commit()
-        self.db.refresh(db_producto)
-        producto.id = db_producto.id
-        return producto
+        
+        try:
+            self.db.add(db_producto)
+            self.db.commit()
+            self.db.refresh(db_producto)
+            producto.id = db_producto.id
+            return producto
+        except Exception as e:
+            self.db.rollback()  # ¡Esta es la magia! Si falla, "limpia" la sesión para que no se trabe.
+            raise e
 
     def obtener_stock(self, nombre_producto: str) -> int | None:
         db_producto = self.db.query(ProductoORM).filter(ProductoORM.nombre == nombre_producto).first()
