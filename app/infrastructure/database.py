e0de156f1ba0dc3e3a0d80a34347453f31ad5bba
Pallas66 <alejandropallares01@gmail.com>
2026-08-20 17:55:57 -0600

    database

--- app/infrastructure/database.py
+++ app/infrastructure/database.py
@@ -1,8 +1,17 @@
+import os
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import declarative_base, sessionmaker
 from app.domain.models import Producto, IProductoRepository
 
-DATABASE_URL = "postgresql://postgres:dcadml25@localhost:5432/inventario_db"
+# 1. Leer la URL desde el archivo .env o desde la nube
+# Render inyectará su propia URL aquí. Si estás en tu PC y no la inyecta, usará tu cadena local como plan B.
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://inventario_db_prod_user:HlcblWrtUaT8RKPwHLJDr1dKY44m0xPC@dpg-da3oq78u01pc73c3qeug-a/inventario_db_prod")
+
+# 2. Validación de seguridad para SQLAlchemy en Render
+# Render a veces entrega URLs que empiezan con 'postgres://'. SQLAlchemy exige 'postgresql://'
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
